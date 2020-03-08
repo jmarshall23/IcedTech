a322e8860b1c057eab1f63f93a7233ea8526adb2
LWSS <bbchallenger100@gmail.com>
2020-03-07 17:23:36 -0800

    launcher use relative path, add dlerror()

--- code/sys/linux/linux_launcher.cpp
+++ code/sys/linux/linux_launcher.cpp
@@ -1,7 +1,7 @@
 #include <dlfcn.h> //dlopen
 #include <cstdio> //printf
 
-#define DLL_NAME "libDoomDLL.so"
+#define DLL_NAME "./libDoomDLL.so"
 
 typedef int (*DoomMainFn)( int, const char ** );
 
@@ -10,14 +10,14 @@ int main( int argc, const char **argv )
     DoomMainFn DoomMain;
     void *doomdll = dlopen(DLL_NAME, RTLD_NOW );
     if( !doomdll ){
-        printf("[Launcher]Couldn't find the Doom dll! (%s)\n", DLL_NAME);
+        printf("[Launcher]Couldn't open the Doom dll! (%s) - error(%s)\n", DLL_NAME, dlerror());
         return 1;
     }
 
     DoomMain = (DoomMainFn)dlsym(doomdll, "DoomMain");
 
     if( !DoomMain ){
-        printf("[Launcher]Couldn't find the DoomMain symbol in Doom dll!\n");
+        printf("[Launcher]Couldn't find the DoomMain symbol in Doom dll! - error(%s)\n", dlerror());
         return 2;
     }
 
