e4972d156d58436f638cf0a95118f819232bdad7
Justin Marshall <justinmarshall20@gmail.com>
2020-04-06 07:26:29 -0700

    Added missing file from previous CL.

---
+++ code/tools/radiant.net/Shared/MenuCommands.cs
@@ -0,0 +1,29 @@
+﻿#if RADIANT_NET
+namespace radiant.net.Shared
+{
+    class MenuCommands
+    {
+        public enum RadiantNetMenuCommands_t {
+#else
+        enum RadiantNetMenuCommands_t {
+#endif
+            // File Menu
+            RADIANT_NET_COMMAND_NEW = 0,
+            RADIANT_NET_COMMAND_OPEN,
+            RADIANT_NET_COMMAND_SAVE,
+            RADIANT_NET_COMMAND_SAVEAS,
+            RADIANT_NET_COMMAND_SAVESELECTION,
+            RADIANT_NET_COMMAND_SAVEREGION,
+            RADIANT_NET_COMMAND_NEWPROJECT,
+            RADIANT_NET_COMMAND_LOADPROJECT,
+            RADIANT_NET_COMMAND_PROJECTSETTINGS,
+            RADIANT_NET_COMMAND_POINTFILE,
+            RADIANT_NET_COMMAND_EXIT
+
+            // Edit Menu
+        };
+#if RADIANT_NET
+    }
+}
+
+#endif
